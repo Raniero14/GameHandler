8cdf23c0119cc601fbec49cd682a947dbab35c24
VLanciotti <valter.lanciotti@itispascal.it>
2022-03-26 10:11:23 +0100

    VL 26.03.2022

--- src/main/java/edu/pascal/Main.java
+++ src/main/java/edu/pascal/Main.java
@@ -5,6 +5,7 @@ import edu.pascal.gamehandler.GameServer;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Avvio GameHandler");
         new GameServer(args);
     }
 
